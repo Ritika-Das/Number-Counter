066f0e6d72fac59381e15a9ec5a2abb16ada1b1d
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2021-03-29 03:50:58 +0530

    Added suggestive text toggle for two modes

--- counter.js
+++ counter.js
@@ -11,10 +11,14 @@ let toggle = () =>
 {
     document.body.style.background = "#000000";
     document.getElementById("H1").style.color = "springgreen";
+    document.getElementById("sp").innerHTML = "Double-click for light mode";
+    document.getElementById("sp").style.color = "#f3f3f3";
 }
 
 let change = () =>
 {
     document.body.style.background = "#f3f3f3";
     document.getElementById("H1").style.color = "black";
+    document.getElementById("sp").innerHTML = "Click to enable Dark Mode";
+    document.getElementById("sp").style.color = "#000000";
 }
